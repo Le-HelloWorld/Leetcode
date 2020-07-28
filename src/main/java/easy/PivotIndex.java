db7336a8a348eb13ee7e804c52abecefd46211c0
Karl.Le <wangle1618@outlook.com>
2020-07-28 08:14:17 +0800

    Leetocde - 寻找中间索引

--- src/main/java/easy/PivotIndex.java
+++ src/main/java/easy/PivotIndex.java
@@ -13,12 +13,19 @@ public class PivotIndex {
         if (nums.length == 1) {
             return 0;
         }
+        if (nums.length == 0) {
+            return -1;
+        }
 
         // right sum
         for (int i : nums) {
             right += i;
         }
         right = right - nums[0];
+
+        if (right == left) {
+            return 0;
+        }
 
         for (int j = 1; j < nums.length; j++)
         {
@@ -32,14 +39,38 @@ public class PivotIndex {
     }
 
     @Test
-    public void test() {
-        int[] test1 = {1, 7, 3, 6, 5, 6};
-        int[] test2 = {1, 2 ,3};
-        int[] test3 = {1, 2 ,1};
-        int[] test4 = {1, 4};
-        Assert.assertEquals(3, pivotIndex(test1));
-        Assert.assertEquals(-1, pivotIndex(test2));
-        Assert.assertEquals(1, pivotIndex(test3));
-        Assert.assertEquals(-1, pivotIndex(test4));
+    public void test1() {
+        int[] test = {1, 7, 3, 6, 5, 6};
+        Assert.assertEquals(3, pivotIndex(test));
+    }
+
+    @Test
+    public void test2() {
+        int[] test = {1, 2 ,3};
+        Assert.assertEquals(-1, pivotIndex(test));
+    }
+
+    @Test
+    public void test3() {
+        int[] test = {1, 2 ,1};
+        Assert.assertEquals(1, pivotIndex(test));
+    }
+
+    @Test
+    public void test4() {
+        int[] test = {1,4};
+        Assert.assertEquals(-1, pivotIndex(test));
+    }
+
+    @Test
+    public void test5() {
+        int[] test = {};
+        Assert.assertEquals(-1, pivotIndex(test));
+    }
+
+    @Test
+    public void test6() {
+        int[] test = {-1 ,-1 , -1, 0, 1, 1};
+        Assert.assertEquals(0, pivotIndex(test));
     }
 }
